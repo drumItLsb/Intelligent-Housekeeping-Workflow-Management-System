011c0e854e12fe5aa0e37e0a01066262b279e897
drumItLsb <likhit.b@kickdrumtech.com>
2026-03-25 14:17:10 +0530

    initialize project

---
+++ src/main/java/com/kickdrum/HousekeepingWorkflowManagementSystem/HousekeepingWorkflowManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kickdrum.HousekeepingWorkflowManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousekeepingWorkflowManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousekeepingWorkflowManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kickdrum/HousekeepingWorkflowManagementSystem/HousekeepingWorkflowManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickdrum.HousekeepingWorkflowManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousekeepingWorkflowManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
