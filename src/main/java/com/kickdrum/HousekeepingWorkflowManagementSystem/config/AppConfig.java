29d56e96232d84b8ba8bd9eca36e6f634242fbda
drumItLsb <likhit.b@kickdrumtech.com>
2026-03-29 13:17:19 +0530

    add cors resolver

--- src/main/java/com/kickdrum/HousekeepingWorkflowManagementSystem/config/AppConfig.java
+++ src/main/java/com/kickdrum/HousekeepingWorkflowManagementSystem/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.kickdrum.HousekeepingWorkflowManagementSystem.exception.CustomAuthent
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,6 +44,7 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
